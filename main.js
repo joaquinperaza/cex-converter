a6c51f5ea078a584b9e26879b889cd3b1bfab046
joaquinperaza <joaquin@peraza.uy>
2017-09-15 17:02:22 -0300

    test1

--- main.js
+++ main.js
@@ -2,36 +2,61 @@ const cheerio = require('cheerio')
 const request = require('superagent');
 
 function converter(options) {
-  return new Promise(function(resolve, reject) {
-    const source = 'https://finance.google.com/finance/converter'
-    const queryString = {
-      a: options.amount,
-      from: options.from,
-      to: options.to
-    }
-    request
-      .get(source)
-      .query(queryString)
-      .end(function(error, response){
-        const $ = cheerio.load(response.text, {normalizeWhitespace: true})
-        const fromResult = $( "select[name='from']" ).val()
-        const toResult = $( "select[name='to']" ).val()
-        const amountResult = $( "input[name='a']" ).val()
-        const converted = $('#currency_converter_result .bld').text()
+    const https = require('https');
 
-        if (!error && response.statusCode == 200) {
-          resolve({
-            from: fromResult,
-            to: toResult,
-            amount: parseFloat(amountResult),
-            converted: parseFloat(converted) || parseFloat(amountResult),
-            url: response.req.url
-          })
-        } else {
-          reject(error);
-        }
-      })
-  })
+    https.get("https://cex.io/api/last_price/BTC/USD", (resp) => {
+        let data = '';
+
+        // A chunk of data has been recieved.
+        resp.on('data', (chunk) => {
+            data += chunk;
+        });
+
+        // The whole response has been received. Print out the result.
+        resp.on('end', () => {
+            var amounto = JSON.parse(data).lprice;//3500USD - 1 BTC
+
+            return new Promise(function (resolve, reject) {
+                const source = 'https://finance.google.com/finance/converter'
+                const queryString = {
+                    a: amounto,
+                    from: "USD",
+                    to: "UYU"
+                }
+                request
+                    .get(source)
+                    .query(queryString)
+                    .end(function (error, response) {
+                        const $ = cheerio.load(response.text, {
+                            normalizeWhitespace: true
+                        })
+                        const fromResult = $("select[name='from']").val()
+                        const toResult = $("select[name='to']").val()
+                        const amountResult = $("input[name='a']").val()
+                        const converted = $('#currency_converter_result .bld').text()
+
+                        if (!error && response.statusCode == 200) {
+                            resolve({
+                                from: fromResult,
+                                to: toResult,
+                                amount: parseFloat(amountResult),
+                                converted: parseFloat(converted) || parseFloat(amountResult),
+                                url: response.req.url
+                            })
+                        } else {
+                            reject(error);
+                        }
+                    })
+            });
+        });
+
+    }).on("error", (err) => {
+        console.log("Error: " + err.message);
+    });
+
+
+
+
 }
 
 module.exports = converter
